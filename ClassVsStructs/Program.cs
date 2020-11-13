2451afbbcd3b4129fdc18c5e8b269a00b96e6bde
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-13 09:19:19 +0100

    Add project files.

---
+++ ClassVsStructs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassVsStructs
+{
+    public class Employee
+    {
+        public string Name;
+        public double Salary;
+
+        public double Update(Employee employee)
+        {
+            double newSalary = 3000.00;
+            return employee.Salary = newSalary;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Salary = 2000.00;
+
+            Console.WriteLine($"Salary before update {employee.Salary}");
+            employee.Update(employee);
+            Console.WriteLine($"Salary after update {employee.Salary}");
+
+        }
+    }
+}
